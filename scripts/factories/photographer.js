286b2df17538b68cf3ef97eb3b537578144e7812
BrandonBissoudre <93727905+BrandonBissoudre@users.noreply.github.com>
2022-04-21 15:03:19 +0200

    ajout de fetch

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,24 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+// function photographerFactory(data) {
+//     const { name, portrait } = data;
+
+//     const picture = `assets/photographers/${portrait}`;
+
+//     function getUserCardDOM() {
+//         const article = document.createElement( 'article' );
+//         const img = document.createElement( 'img' );
+//         img.setAttribute("src", picture)
+//         const h2 = document.createElement( 'h2' );
+//         h2.textContent = name;
+//         article.appendChild(img);
+//         article.appendChild(h2);
+//         return (article);
+//     }
+//     return { name, picture, getUserCardDOM }
+// }
+
+
+function photographerFactory(data)  {
+    const { name, portrait, city, country, tagline } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -11,7 +30,12 @@ function photographerFactory(data) {
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
+        const p = document.createElement('p');
+        p.textContent = city + country + tagline
+        article.appendChild(p)
+        
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    return { name, picture, getUserCardDOM }     
 }
