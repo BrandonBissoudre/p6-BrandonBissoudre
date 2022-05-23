72b527f347f9c1bbb3b329ebf36827b253037407
BrandonBissoudre <93727905+BrandonBissoudre@users.noreply.github.com>
2022-05-23 19:14:12 +0200

    Creation page profil + correction de code

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,34 +1,15 @@
-// function photographerFactory(data) {
-//     const { name, portrait } = data;
-
-//     const picture = `assets/photographers/${portrait}`;
-
-//     function getUserCardDOM() {
-//         const article = document.createElement( 'article' );
-//         const img = document.createElement( 'img' );
-//         img.setAttribute("src", picture)
-//         const h2 = document.createElement( 'h2' );
-//         h2.textContent = name;
-//         article.appendChild(img);
-//         article.appendChild(h2);
-//         return (article);
-//     }
-//     return { name, picture, getUserCardDOM }
-// }
-
-
-function photographerFactory(data)  {
+function photographerFactory(data) {
     const { name, portrait, city, country, tagline, id, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const lien = document.createElement ('a')
-        lien.setAttribute("href", "photographer.html?id="+id)
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const lien = document.createElement('a')
+        lien.setAttribute("href", "photographer.html?id=" + id)
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
@@ -41,29 +22,28 @@ function photographerFactory(data)  {
         const p3 = document.createElement('p');
         p3.textContent = price
         article.appendChild(p3)
-        
+
 
         lien.appendChild(article)
-        
+
         return (lien);
     }
 
-    return { name, picture, getUserCardDOM }     
+    return { name, picture, getUserCardDOM }
 }
 
-
 // Profil photographer.html //
-function profil(data){
+function profil(data) {
     const { name, portrait, city, country, tagline, } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-       
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
@@ -75,11 +55,54 @@ function profil(data){
         p2.textContent = tagline
         p2.classList.add("textp2")
         article.appendChild(p2)
-        
- 
+
+
         return (article);
     }
 
-    return { name, picture, getUserCardDOM } 
-    
+    return { name, picture, getUserCardDOM }
+
+}
+
+function contentCard(data) {
+    const { id, title, image, video, likes, date } = data;
+
+    const picturePath = `assets/photo/${image}`;
+    const videoPath = `assets/photo/${video}`;
+
+    function getCardDOM() {
+
+        const card = document.createElement('div');
+        card.classList.add("photograph-card")
+        if (image) {
+            const img = document.createElement('img');
+            img.setAttribute("src", picturePath)
+            card.appendChild(img);
+
+        } else if (video) {
+            const video = document.createElement('video');
+            video.setAttribute("src", videoPath)
+            video.autoplay = true;
+            card.appendChild(video);
+        }
+        const info = document.createElement('div')
+        info.classList.add("card-info")
+        const titleSpan = document.createElement('span');
+        titleSpan.textContent = title
+        info.appendChild(titleSpan);
+        const likesSpan = document.createElement('span');
+        likesSpan.innerText = likes
+        const heartSvg = document.createElement('img');
+        heartSvg.setAttribute("src", "assets/icons/heart.svg")
+        heartSvg.classList.add('heart-icon')
+        likesSpan.appendChild(heartSvg)
+        likesSpan.classList.add("likes")
+        info.appendChild(likesSpan)
+        card.appendChild(info)
+
+        return (card);
+    }
+
+    return { name, picture: picturePath, getCardDOM }
+
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,51 +1,123 @@
 //Mettre le code JavaScript lié à la page photographer.html
+window.addEventListener('load', () => {
 
-const queryString = window.location.search;
-console.log(queryString);
+    let media = [];
+    let photographersContentSection
 
-const urlParams = new URLSearchParams(queryString);
+    async function init() {
+        // Récupération de l'id
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const photographId = urlParams.get('id');
 
-const id = urlParams.get('id')
-console.log(id);
+        // Display Content
+        displayHeader(photographId);
+        displayContent(photographId);
 
-async function getPhotographers() {
+        // Filter init
+        initFilters();
+    };
 
-    const photographer = fetchphotographer().then(photographer => {
-     console.log(photographer)
-      return photographer;
-     });
+    async function displayHeader(photographId) {
+        const photograph = await getPhotographerById(photographId);
 
-     return photographer
-    
+        const photographersSection = document.querySelector(".photograph-header");
+        const photographerModel = profil(photograph);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    };
+
+    async function displayContent(photographId) {
+        photographersContentSection = document.querySelector(".photograph-content");
+        media = await getPhotographerContentById(photographId);
+        // Render with asc popularity by default
+        media = media.sort((a, b) => b.likes - a.likes)
+
+        renderMedia();
+    };
+
+    function renderMedia() {
+        photographersContentSection.innerHTML = "";
+        media.forEach(x => {
+            const card = contentCard(x);
+            const cardDOM = card.getCardDOM();
+            photographersContentSection.appendChild(cardDOM);
+        })
     }
 
-   async function displayData(photographers) {
-       const photographersSection = document.querySelector(".photograph-header");
-       console.log (photographers)
-    // refaire pour recup par id //
-       photographers.forEach((photographer) => {
-           if(id==photographer.id) {
-           const photographerModel = profil(photographer);
-           const userCardDOM = photographerModel.getUserCardDOM();
-           photographersSection.appendChild(userCardDOM);
-           }         
-       });
-   };
+    async function getPhotographerById(id) {
+        const response = await fetch("./data/photographers.json");
+        const photographer = await response.json();
+        return photographer.photographers.find(x => x.id === +id);
+    }
 
-   async function init() {
-       // Récupère les datas des photographes
-      const photographers = await getPhotographers();
-       displayData(photographers);
-   };
-   
-   init();
-   
-//fonction fetch 
+    async function getPhotographerContentById(id) {
+        const response = await fetch("./data/photographers.json");
+        const photographer = await response.json();
+        return photographer.media.filter(x => x.photographerId === +id);
+    }
 
-   async function fetchphotographer(){
-     const response = await fetch("./data/photographers.json");
-     const photographer = await response.json();
-     return photographer.photographers;
-   }
+    function initFilters() {
+        const filterMenu = document.getElementById('filter-menu')
+        filterMenu.addEventListener('click', (x) => {
+            const element = x.target;
+            const isSelected = element.classList.length > 0;
+            if (isSelected) {
+                const isUp = element.classList[0].indexOf('dd-button-selected-up') === 0;
+                if (isUp) {
+                    element.classList.remove('dd-button-selected-up');
+                    element.classList.add('dd-button-selected-down');
+                    // handle up 
+                    filterMedia(element.id, false);
+                } else {
+                    element.classList.remove('dd-button-selected-down');
+                    element.classList.add('dd-button-selected-up');
+                    filterMedia(element.id, true);
+                }
+            } else {
+                const items = filterMenu.getElementsByTagName("li");
+                for (var i = 0; i < items.length; ++i) {
+                    items[i].classList.remove('dd-button-selected-up');
+                    items[i].classList.remove('dd-button-selected-down');
+                }
+                element.classList.add('dd-button-selected-up');
+                filterMedia(element.id, true);
+            }
+        })
+    }
 
- 
+    function filterMedia(filter, isUp) {
+        switch (filter) {
+            case 'popularity':
+                if (isUp) {
+                    media = media.sort((a, b) => b.likes - a.likes)
+                    renderMedia();
+                } else {
+                    media = media.sort((a, b) => a.likes - b.likes)
+                    renderMedia();
+                }
+                break;
+            case 'date':
+                if (isUp) {
+                    media = media.sort((a, b) => new Date(b.date) - new Date(a.date))
+                    renderMedia();
+                } else {
+                    media = media.sort((a, b) => new Date(a.date) - new Date(b.date))
+                    renderMedia();
+                }
+                break;
+            case 'title':
+                if (isUp) {
+                    media = media.sort((a, b) => a.title.localeCompare(b.title))
+                    renderMedia();
+
+                } else {
+                    media = media.sort((a, b) => b.title.localeCompare(a.title))
+                    renderMedia();
+                }
+                break;
+        }
+    }
+
+    init();
+})
